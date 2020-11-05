1f5da16c926f098391b415e2d23c1c8ef7b2bc51
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2020-11-05 13:35:05 -0500

    Fix header logging mistype

--- cxt.py
+++ cxt.py
@@ -225,7 +225,7 @@ class AssetHeader(Object):
     def __repr__(self):
         return "<AssetHeader: type: 0x{:0>4x}, id: 0x{:0>4x} ({:0>4d}){}{}>".format(
             self.type.d, self.id.d, self.id.d,
-            " {}".format(self.ref.d if self.ref else ""),
+            " {}".format(self.ref.d) if self.ref else "",
             ", name: {}".format(self.name.d) if self.name else ""
         )
 
