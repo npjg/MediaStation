1af7c6c21d5a7cb7e79e43741e77039f1a380d41
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2024-05-12 13:25:56 -0400

    Allow installing the package even when a C compiler isn't available.

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
 from setuptools import Extension, setup
 
-setup(
-    ext_modules = [
-        Extension(name = 'MediaStationBitmapRle', sources = ['MediaStation/Assets/BitmapRle.c'])
-    ]
-)
+import warnings
+
+# TODO: Distribute prebuilt wheels for the C bitmap decompression accelerator extension.
+#
+# For development work, looks like you would use this to compile the 
+# C-based image decompressor:
+#  python3 setup.py build_ext --inplace
+bitmap_decompression = Extension(name = 'MediaStationBitmapRle', sources = ['MediaStation/Assets/BitmapRle.c'])
+try:
+    # TRY TO COMPILE THE C-BASED IMAGE DECOMPRESSOR.
+    setup(
+        name = 'MediaStation',
+        ext_modules = [bitmap_decompression])
+except:
+    # RELY ON THE PYTHON FALLBACK.
+    warnings.warn('The C bitmap decompression binary is not available on this installation. Expect image decompression to be SLOW.')
+    setup(name = 'MediaStation')
