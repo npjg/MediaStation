5bcb98b1082819c951f75cb67c49b4b9fa95e066
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2024-06-02 15:14:48 -0400

    Improve section type stream variable name.
    Raise a BinaryParsingError when there's an unknown section type.

--- src/MediaStation/Assets/Asset.py
+++ src/MediaStation/Assets/Asset.py
@@ -4,6 +4,7 @@ from enum import IntEnum
 import os
 
 from asset_extraction_framework.Asserts import assert_equal
+from asset_extraction_framework.Exceptions import BinaryParsingError
 
 from ..Primitives.Datum import Datum
 from ..Primitives.Polygon import Polygon
@@ -101,23 +102,23 @@ class Asset:
     ##  - MOVIE
     ##  - UNK2
     ## \param[in] stream - A binary stream that supports the read method.
-    def __init__(self, stream):
+    def __init__(self, chunk):
         # READ THE UNIVERSAL MEMBERS FOR THIS ASSET.
         # These members are always present in all asset headers.
         #
         # The file number of the context that contains this asset.
         # For instance, an asset in file "100.CXT" would have file number 100.
-        self.file_number: int = Datum(stream).d
+        self.file_number: int = Datum(chunk).d
         self.name = None
         self.unks = []
 
         # READ THE ASSET TYPE.
-        self.type = Asset.AssetType(Datum(stream).d)
+        self.type = Asset.AssetType(Datum(chunk).d)
         if (Asset.AssetType.STAGE == self.type):
             self.children = None
 
         # The ID of this asset. Unique within each game.
-        self.id: int = Datum(stream).d
+        self.id: int = Datum(chunk).d
 
         # The asset ID of the stage on which this asset is shown.
         # None if the asset does not belong to a stage.
@@ -130,14 +131,14 @@ class Asset:
         # when we are considering games that run different versions of the engine,
         # it was simplest to read each of the sections in a loop and populate
         # whatever the asset header contains.
-        section_type: int = Datum(stream).d
+        section_type: int = Datum(chunk).d
         more_sections_to_read = (Asset.SectionType.EMPTY != section_type)
         while more_sections_to_read:
             # READ THE CURRENT SECTION.
-            self._read_section(section_type, stream)
+            self._read_section(section_type, chunk)
 
             # READ THE NEXT SECTION TYPE.
-            section_type = Datum(stream).d
+            section_type = Datum(chunk).d
             more_sections_to_read = (Asset.SectionType.EMPTY != section_type)
 
         # CREATE THE CHILDREN.
@@ -157,22 +158,22 @@ class Asset:
             self.movie = Movie(self)
 
     ## Reads all the various sections that can occur in an asset header.
-    def _read_section(self, section_type, stream):
+    def _read_section(self, section_type, chunk):
         if Asset.SectionType.BYTECODE_TRIGGER == section_type: # TMR, MOV
             # READ A BYTECODE TRIGGER.
             # TODO: Define what this is.
-            script = Script(stream, in_independent_asset_chunk = False)
+            script = Script(chunk, in_independent_asset_chunk = False)
             self.functions.append(script)
 
         elif Asset.SectionType.STAGE == section_type: # All
             # READ A STAGE ASSET ID.
-            self.stage_id = Datum(stream).d
+            self.stage_id = Datum(chunk).d
 
         elif Asset.SectionType.ASSET_ID == section_type:
             # READ THE ASSET ID.
             # We already have this asset's ID, so we will just verify it is the same
             # as the ID we have already read.
-            duplicate_asset_id = Datum(stream).d
+            duplicate_asset_id = Datum(chunk).d
             assert_equal(duplicate_asset_id, self.id, 'asset ID')
 
         elif Asset.SectionType.CHUNK_REFERENCE == section_type: # SND, IMG, SPR, MOV, FON
@@ -185,34 +186,34 @@ class Asset:
             # - They might be in a different CXT file entirely.
             if self.type == Asset.AssetType.MOVIE:
                 # READ THE HEADER REFERENCE.
-                header_reference = Datum(stream).d.chunk_id
+                header_reference = Datum(chunk).d.chunk_id
                 self.chunk_references.append(header_reference)
                 # TODO: Find out what this is and why we have to skip it.
-                self.unks.append({hex(section_type): Datum(stream).d})
+                self.unks.append({hex(section_type): Datum(chunk).d})
 
                 # READ THE AUDIO REFERENCE.
-                audio_reference = Datum(stream).d.chunk_id
+                audio_reference = Datum(chunk).d.chunk_id
                 self.chunk_references.append(audio_reference)
                 # TODO: Find out what this is and why we have to skip it.
-                self.unks.append({hex(section_type): Datum(stream).d})
+                self.unks.append({hex(section_type): Datum(chunk).d})
 
                 # READ THE VIDEO REFERENCE.
-                video_reference = Datum(stream).d.chunk_id
+                video_reference = Datum(chunk).d.chunk_id
                 self.chunk_references.append(video_reference)
                 # TODO: Find out what this is and why we have to skip it.
             else: 
                 # READ A SINGLE REFERENCE.
-                chunk_reference = Datum(stream).d.chunk_id
+                chunk_reference = Datum(chunk).d.chunk_id
                 self.chunk_references.append(chunk_reference)
 
         elif Asset.SectionType.BOUNDING_BOX == section_type: # STG, IMG, HSP, SPR, MOV, TXT, CAM, CVS
-            self.bounding_box = Datum(stream).d
+            self.bounding_box = Datum(chunk).d
 
         elif Asset.SectionType.POLYGON == section_type:
-            self.mouse_active_area_polygon = Polygon(stream)
+            self.mouse_active_area_polygon = Polygon(chunk)
 
         elif Asset.SectionType.Z_INDEX == section_type:
-            self.z_index = Datum(stream).d
+            self.z_index = Datum(chunk).d
 
         elif section_type == Asset.SectionType.ASSET_REFERENCE: # IMG
             # This is the asset ID of the asset that has the same 
@@ -222,95 +223,99 @@ class Asset:
             # in the asset img_7x51gg009all_GearThreeHigh. 
             # I don't know why they did it this way, as just the chunk
             # reference could have been the same rather than an entire asset reference.
-            self.asset_reference = Datum(stream).d
+            self.asset_reference = Datum(chunk).d
 
         elif section_type == 0x001f: # IMG, HSP, SPR, MOV, TXT, CVS
-            self.automatically_play = bool(Datum(stream).d)
+            self.automatically_play = bool(Datum(chunk).d)
 
         elif section_type == 0x0020: # IMG, SPR, CVS
             # TODO: Determine what this is.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0021: # SND, MOV
-            self._has_own_subfile = bool(Datum(stream).d)
+            self._has_own_subfile = bool(Datum(chunk).d)
 
         elif section_type == 0x0022: # SCR, TXT, CVS
             # TODO: Determine what this is.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0024: # SPR
             # TODO: Determine what this is.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
+
+        #elif section_type == 0x0026: # TXT
+        #    # TODO: This seems to only occur in Ariel.
+        #    self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0032: # IMG, SPR
             # TODO: Determine what this is.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0033: # SND, MOV
             # READ THE 
             # These assets are 
-            self.total_chunks = Datum(stream).d
-            self.rate = Datum(stream).d
+            self.total_chunks = Datum(chunk).d
+            self.rate = Datum(chunk).d
 
         elif section_type == 0x0034:
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0037:
             # TODO: Determine what this is.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0258: # TXT
             # This should always be the first entry
             # that defines a text stream.
             self.text = Text.Text()
-            self.text.font_asset_id = Datum(stream).d
+            self.text.font_asset_id = Datum(chunk).d
 
         elif section_type == 0x0259: # TXT
-            self.text.initial_text = Datum(stream).d
+            self.text.initial_text = Datum(chunk).d
 
         elif section_type == 0x025a: # TXT
-            self.text.maximum_width_in_pixels = Datum(stream).d
+            self.text.maximum_width_in_pixels = Datum(chunk).d
 
         elif section_type == 0x025b: # TXT
-            self.text.justification = Text.Justification(Datum(stream).d)
+            self.text.justification = Text.Justification(Datum(chunk).d)
 
         elif section_type == 0x025f: # TXT
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0262: # TXT
-            self.unks.append({hex(section_type): Datum(stream).d})
+            pass
 
         elif section_type == 0x0263:
             pass
 
         elif section_type == 0x0265: # TXT
-            [Datum(stream) for _ in range(3)]
+            [Datum(chunk) for _ in range(3)]
 
         elif section_type == 0x0266:
-            [Datum(stream) for _ in range(2)]
+            [Datum(chunk) for _ in range(2)]
 
         elif section_type >= 0x3a98 and section_type <= 0x3afb:
             self.id = section_type
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x03e8: # SPR
-            self.chunks = Datum(stream).d
+            self.chunks = Datum(chunk).d
 
         elif section_type == 0x03e9: # SPR
             self.mouse = {"frames": [], "first": None}
             self.mouse["frames"].append(
-                {"id": Datum(stream).d, "x": Datum(stream).d, "y": Datum(stream).d}
+                {"id": Datum(chunk).d, "x": Datum(chunk).d, "y": Datum(chunk).d}
             )
 
         elif section_type == 0x03ea: # SPR
-            self.mouse["first"] = Datum(stream).d
+            self.mouse["first"] = Datum(chunk).d
 
         elif section_type == 0x03eb: # IMG, SPR, TXT, CVS
-            self.editable = bool(Datum(stream).d)
+            self.editable = bool(Datum(chunk).d)
 
         elif section_type == 0x03ec:
             # This should only occur in version 1 games.
-            self.cursor = Datum(stream).d
+            self.cursor = Datum(chunk).d
 
         elif section_type == 0x03ed:
             # This should only occur in version 1 games.
@@ -319,120 +324,116 @@ class Asset:
             # so it's okay to hardcode the constant 5.
             self.mouse = {
                 "timers": {
-                    Datum(stream).d: [Datum(stream).d, Datum(stream).d] for _ in range(5)
+                    Datum(chunk).d: [Datum(chunk).d, Datum(chunk).d] for _ in range(5)
                 }
             }
 
         elif section_type == 0x03ee:
-            self.mouse.update({"unk": [Datum(stream).d, Datum(stream).d]})
+            self.mouse.update({"unk": [Datum(chunk).d, Datum(chunk).d]})
 
         elif section_type == 0x03ef:
             # This should only occur in version 1 games.
             if not self.mouse.get("barriers"):
                 self.mouse.update({"barriers": []})
 
-            self.mouse["barriers"].append(Datum(stream).d)
+            self.mouse["barriers"].append(Datum(chunk).d)
 
         elif section_type >= 0x03f0 and section_type <= 0x3f5:
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type >= 0x0514 and section_type < 0x0519:
             # These data are constant across the LKASB constellation
             # minigame. I will ignore them.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0519:
             # Same comment as above.
             for _ in range(3):
-                self.unks.append({hex(section_type): Datum(stream).d})
+                self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x05aa: # PAL
-            self.palette = stream.read(0x300)
+            self.palette = chunk.read(0x300)
 
         elif section_type == 0x05dc:
             # It's only not 0.0 in the 'Read to me' and 'Read and play'
             # images of Dalmatians. So I will ignore it.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x05dd:
             # I can't find an instance where this isn't 1. So I will ignore it.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x05de: # IMG
-            self.x = Datum(stream).d
+            self.x = Datum(chunk).d
 
         elif section_type == 0x05df: # IMG
-            self.y = Datum(stream).d
+            self.y = Datum(chunk).d
 
         elif section_type == 0x060e: # PTH
-            self.start = [Datum(stream).d]
+            self.start = [Datum(chunk).d]
 
         elif section_type == 0x060f: # PTH
-            self.end = [Datum(stream).d]
+            self.end = [Datum(chunk).d]
 
         elif section_type == 0x0610: # PTH
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0611: # PTH
-            self.end.append(Datum(stream).d)
+            self.end.append(Datum(chunk).d)
 
         elif section_type == 0x0612: # PTH
-            self.start.append(Datum(stream).d)
+            self.start.append(Datum(chunk).d)
 
         elif section_type == 0x06ac:
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x076f: # CAM
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0770: # CAM
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0772: # STG
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x774: # IMAGE_SET
-            self.bitmap_count = Datum(stream).d
+            self.bitmap_count = Datum(chunk).d
 
         elif section_type == 0x775: # IMAGE_SET
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x776: # IMAGE_SET
             # I think this is just a marker for the beginning of the image set
             # data (0x778s), so I think we can just ignore this.
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
             self.bitmap_declarations = []
 
         elif section_type == 0x778: # IMAGE_SET
-            bitmap_declaration = BitmapSetBitmapDeclaration(stream)
+            bitmap_declaration = BitmapSetBitmapDeclaration(chunk)
             self.bitmap_declarations.append(bitmap_declaration)
 
         elif section_type == 0x779: # IMAGE_SET
             # TODO: Figure out what this is. I just split it out here so I 
             # wouldn't forget about it.
-            self.unk_bitmap_set_bounding_box = Datum(stream).d
+            self.unk_bitmap_set_bounding_box = Datum(chunk).d
 
         elif section_type >= 0x0773 and section_type <= 0x0780:
-            self.unks.append({hex(section_type): Datum(stream).d})
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
-        elif section_type == 0x2734:
-            self.unks.append({hex(section_type): Datum(stream).d})
-
-        elif section_type == 0x27b2:
-            self.unks.append({hex(section_type): Datum(stream).d})
+        elif section_type >= 0x2734 and section_type <= 0x2800:
+            self.unks.append({hex(section_type): Datum(chunk).d})
 
         elif section_type == 0x0bb8:
             # READ THE ASSET NAME.
-            self.name = Datum(stream).d
+            self.name = Datum(chunk).d
 
         elif section_type == 0x0001: # SND
-            self.audio_encoding = Datum(stream).d
+            self.audio_encoding = Datum(chunk).d
 
         elif section_type == 0x0002: # SND
             # TODO: Determine what the dfference is between the previous
             # audio encoding section type and this one.
-            self.audio_encoding = Datum(stream).d
+            self.audio_encoding = Datum(chunk).d
 
         else:
-            # INDICATE AN ERROR.
-            raise TypeError(f'Unknown section type: 0x{section_type:0>4x}')
+            raise BinaryParsingError(f'Unknown section type: 0x{section_type:0>4x}', chunk.stream)
