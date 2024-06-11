2e62ea981b0c9af3fd158a79aca0f6a0e05e15d4
Nathanael Gentry <nathanael.gentrydb8@gmail.com>
2024-06-11 17:46:11 -0400

    Work around random segfaults by reloading the engine after each title.

--- tests/test_game_parsing.py
+++ tests/test_game_parsing.py
@@ -3,16 +3,17 @@ import shutil
 import tempfile
 import subprocess
 import warnings
+import importlib
 
 import pytest
 
 from MediaStation import Engine 
 
-# Currently, asset export is SLOW because the decompression logic is
-# implemented in pure Python, not C. However, bugs are more likely 
-# to occur in parsing (as opposed to exporting), so to run tests faster
-# we disable asset exporting.
-ENABLE_ASSET_EXPORT_IN_TESTS = False
+
+# Even with the C-based decompression exporting ALL assets
+# takes a while, so it probably only needs to be run right
+# before a release, not after every commit.
+ENABLE_ASSET_EXPORT_IN_TESTS = True
 
 # The tests MUST be run from the root of the repository.
 GAME_ROOT_DIRECTORY = 'tests/test_data/Extracted Folders'
@@ -59,12 +60,23 @@ def test_script_is_runnable():
 
 @pytest.mark.parametrize("game_directory_path", game_directories)
 def test_process_game(game_directory_path):
+    # RELOAD THE ENGINE.
+    # If this is not done, segfaults seem to happen once we have
+    # exported enough titles. Since this doesn't happen when 
+    # importing individual files, we will just reload the engine
+    # each time as a workaround.
+    importlib.reload(Engine)
+
     # PARSE THE RESOURCES.
     print(game_directory_path)
     if ENABLE_ASSET_EXPORT_IN_TESTS:
         temp_dir = tempfile.mkdtemp()
         try:
-            Engine.main([game_directory_path, '--export', temp_dir])
+            # We will skip the metadata export because it is currently SLOW,
+            # and bugs aren't really expected to show up there if everything 
+            # else succeeds.
+            Engine.main([game_directory_path, '--export', temp_dir, "--skip-metadata-export"])
+            
             # TODO: Do something to verify the integrity of the created files
             # before we delete them.
         finally:
